db2c4b4fbabbb47fcbf028d8d454adb2aa736881
Karen Zhao <karen.zhao18@gmail.com>
2018-08-21 12:49:02 -0400

    setup apollo client

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { ApolloProvider } from 'react-apollo';
+import client from './apolloClient';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Movies
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          Movies
+        </div>
+      </ApolloProvider>
     );
   }
 }
